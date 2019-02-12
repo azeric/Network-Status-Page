dcac685b5449b67b1f6d415c22a1d39ec19ed0f9
Eric Charlson <Eric.Charlson@confirmit.com>
2019-02-12 13:19:43 -0500

    vnstat installed

--- assets/php/functions.php
+++ assets/php/functions.php
@@ -498,9 +498,9 @@ function getBandwidth()
 	global $ssh_username;
 	global $ssh_password;
 	global $pfsense_if_name;
-	$ssh = new Net_SSH2($local_pfsense_ip);
-	if (!$ssh->login($ssh_username,$ssh_password)) { // replace password and username with pfSense ssh username and password if you want to use this
-		exit('Login Failed');
+	//$ssh = new Net_SSH2($local_pfsense_ip);
+	//if (!$ssh->login($ssh_username,$ssh_password)) { // replace password and username with pfSense ssh username and password if you want to use this
+	//	exit('Login Failed');
 	}
 
 	$dump = shell_exec('vnstat -i '.$pfsense_if_name.' -tr');
