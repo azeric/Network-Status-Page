56652e7001362f8d84e8b5e8bfe6558138055171
Eric Charlson <Eric.Charlson@confirmit.com>
2019-02-13 14:14:31 -0500

    fix curl

--- assets/php/service.class.php
+++ assets/php/service.class.php
@@ -19,7 +19,7 @@ class service
 	function check_port()
 	{
 		//if (!$fp = curl_init($this->url)) return false;
-		$headers=get_headers($url, 1);
+		$headers=get_headers($this->url, 1);
    		if ($headers[0]!='HTTP/1.1 200 OK') return true; else return false;
 	}
 	
