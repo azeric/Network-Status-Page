87c5cede1937e0b9d3d4c99a290e82d04dcbdb0e
Eric Charlson <Eric.Charlson@confirmit.com>
2019-02-20 13:12:10 -0500

    cleanup

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
 	include('assets/php/Mobile_Detect.php');
 
 	$detect = new Mobile_Detect;
-	//$plexSessionXML = simplexml_load_file($config['network']['plex_server_ip'].'/status/sessions');
 ?>
 <html lang="en">
 	<head>
