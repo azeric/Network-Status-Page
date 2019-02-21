d762f2451c44b081cf12f25ef0549760620ebd37
Eric Charlson <Eric.Charlson@confirmit.com>
2019-02-21 11:43:05 -0500

    back to curl

--- assets/php/service.class.php
+++ assets/php/service.class.php
@@ -18,20 +18,17 @@ class service
 	
 	function check_port()
 	{
-		// $handle = curl_init($this->url);
-		// curl_setopt($handle,  CURLOPT_RETURNTRANSFER, TRUE);
+		$handle = curl_init($this->url);
+		curl_setopt($handle,  CURLOPT_RETURNTRANSFER, TRUE);
 
-		// $response = curl_exec($handle);
+		$response = curl_exec($handle);
 
-		// /* Check for 404 (file not found). */
-		// $httpCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);
-		// if($httpCode == 404) {
-		// 	return false;
-		// }
-		// return true;
-
-		$headers=get_headers($this->url, 1);
-   		if ($headers[0]!='HTTP/1.1 200 OK') return true; else return false;
+		/* Check for 404 (file not found). */
+		$httpCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+		if($httpCode == 404) {
+			return false;
+		}
+		return true;
 
 curl_close($handle);
 	}
