1e63a7f36d471c433452bd121001298a28010afb
Eric Charlson <Eric.Charlson@confirmit.com>
2019-02-21 13:22:54 -0500

    print_r

--- assets/php/service.class.php
+++ assets/php/service.class.php
@@ -28,7 +28,7 @@ class service
 		/* Check for 404 (file not found). */
 		$httpCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);
 		$error = curl_getinfo($handle, CURLINFO_NAMELOOKUP_TIME);
-		echo "console.log(".$error.");";
+		print_r($error);
 		if($httpCode == 404) {
 			return false;
 		}
