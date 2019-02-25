591056c45757b6ed6d77e0e98a9d83cf1cb914ac
Eric Charlson <Eric.Charlson@confirmit.com>
2019-02-25 13:36:42 -0500

    add all icons

--- assets/php/services_ajax.php
+++ assets/php/services_ajax.php
@@ -11,17 +11,17 @@
 <?php 
 $services = array(
 	new service("Plex", "https://videos.01tools.com/web/index.html#!/dashboard", "../img/plex.ico" ),
-	new service("NextCloud", "https://cloud.01tools.com"),
-	new service("Home Assistant", "https://iot.01tools.com"),
-	new service("Motioneye", "https://camera.01tools.com"),
-	new service("ruTorrent", "https://lw815.ultraseedbox.com/~azeric/rutorrent/"),
-	new service("Sonarr","https://tv.01tools.com"),
-	new service("Radarr", "https://movies.01tools.com"),
-	new service("Jackett", "https://indexer.01tools.com"),
-	new service("PiHole", "https://dns.01tools.com/admin"),
-	new service("Traefik", "https://proxy.01tools.com:8081/"),
-	new service("Grafana", "https://graphs.01tools.com/"),
-	new service("Trakt", "https://trakt.tv/dashboard")	
+	new service("NextCloud", "https://cloud.01tools.com", "../img/plex.ico"),
+	new service("Home Assistant", "https://iot.01tools.com", "../img/plex.ico"),
+	new service("Motioneye", "https://camera.01tools.com", "../img/plex.ico"),
+	new service("ruTorrent", "https://lw815.ultraseedbox.com/~azeric/rutorrent/", "../img/plex.ico"),
+	new service("Sonarr","https://tv.01tools.com", "../img/plex.ico"),
+	new service("Radarr", "https://movies.01tools.com", "../img/plex.ico"),
+	new service("Jackett", "https://indexer.01tools.com", "../img/plex.ico"),
+	new service("PiHole", "https://dns.01tools.com/admin", "../img/plex.ico"),
+	new service("Traefik", "https://proxy.01tools.com:8081/", "../img/plex.ico"),
+	new service("Grafana", "https://graphs.01tools.com/", "../img/plex.ico"),
+	new service("Trakt", "https://trakt.tv/dashboard", "../img/plex.ico")	
 );
 ?>
 <table class ="center">
