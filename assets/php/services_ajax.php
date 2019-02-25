87e7f6896b311980cef7722f044fd4e975c738a0
Eric Charlson <Eric.Charlson@confirmit.com>
2019-02-25 13:48:27 -0500

    a

--- assets/php/services_ajax.php
+++ assets/php/services_ajax.php
@@ -27,7 +27,7 @@ $services = array(
 <table class ="center">
 	<?php foreach($services as $service){ ?>
 		<tr>
-			<td style="text-align: right; padding-right:5px;" class="exoextralight"><img src="<?php echo $service->icon; ?>" style="width:14px height:14px" /><?php echo $service->name; ?></td>
+			<td style="text-align: right; padding-right:5px;" class="exoextralight"><img src="<?php echo $service->icon; ?>" style="width:14px !important height:14px" /><?php echo $service->name; ?></td>
 			<td style="text-align: left;"><?php echo $service->makeButton(); ?></td>
 		</tr>
 	<?php }?>
