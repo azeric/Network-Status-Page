6627f2d948bc81bfff069767d5b2a13c6d68c1ff
Eric Charlson <Eric.Charlson@confirmit.com>
2019-06-05 13:43:15 -0400

    node-red icon

--- assets/php/services_ajax.php
+++ assets/php/services_ajax.php
@@ -8,13 +8,13 @@
 <?php 
 $services = array(
 	new service("Plex", "https://videos.01tools.com/web/index.html#!/dashboard", "/assets/img/plex.ico" ),
-	new service("NextCloud", "https://cloud.01tools.com", "/assets/img/nextcloud.png"),
-	new service("Home Assistant", "https://iot.01tools.com", "/assets/img/homeassistant.png"),
-	new service("Node Red", "https://automate.01tools.com", "/assets/img/homeassistant.png"),
-	new service("Motioneye", "https://camera.01tools.com", "/assets/img/motioneye.png"),
-	new service("ruTorrent", "https://lw815.ultraseedbox.com/~azeric/rutorrent/", "/assets/img/rutorrent.png"),
 	new service("Sonarr","https://tv.01tools.com", "/assets/img/sonarr.ico"),
 	new service("Radarr", "https://movies.01tools.com", "/assets/img/radarr.png"),
+	new service("ruTorrent", "https://lw815.ultraseedbox.com/~azeric/rutorrent/", "/assets/img/rutorrent.png"),
+	new service("Home Assistant", "https://iot.01tools.com", "/assets/img/homeassistant.png"),
+	new service("Motioneye", "https://admincamera.01tools.com", "/assets/img/motioneye.png"),
+	new service("Node Red", "https://automate.01tools.com", "/assets/img/node-red.png"),
+	new service("NextCloud", "https://cloud.01tools.com", "/assets/img/nextcloud.png"),	
 	new service("Jackett", "https://indexer.01tools.com", "/assets/img/jackett.png"),
 	new service("PiHole", "https://dns.01tools.com/admin", "/assets/img/pihole.png"),
 	new service("Traefik", "https://proxy.01tools.com/", "/assets/img/traefik.png"),
