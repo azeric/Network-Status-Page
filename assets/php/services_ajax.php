cd4b3105fe1a1f5fa329e1b65f57d880cdd8ee8c
Eric Charlson <Eric.Charlson@confirmit.com>
2020-02-20 13:45:33 -0500

    add spaces

--- assets/php/services_ajax.php
+++ assets/php/services_ajax.php
@@ -7,19 +7,19 @@
 <html lang="en">
 <?php 
 $services = array(
-	new service("Plex", "https://videos.01tools.com/web/index.html#!/dashboard", "/assets/img/plex.ico" ),
-	new service("Sonarr","https://tv.01tools.com", "/assets/img/sonarr.ico"),
-	new service("Radarr", "https://movies.01tools.com", "/assets/img/radarr.png"),
-	new service("Deluge", "https://azeric.lw815.usbx.me/deluge", "/assets/img/deluge.png"),
-	new service("Home Assistant", "https://iot.01tools.com", "/assets/img/homeassistant.png"),
-	new service("Motioneye", "https://camera.01tools.com", "/assets/img/motioneye.png"),
-	new service("Node Red", "https://automate.01tools.com", "/assets/img/node-red.png"),
-	new service("NextCloud", "https://cloud.01tools.com", "/assets/img/nextcloud.png"),	
-	new service("Bitwarden", "https://pass.01tools.com/", "/assets/img/bitwarden.png"),
-	new service("Authelia", "https://login.01tools.com/", "/assets/img/authelia.png"),
-	new service("Jackett", "https://indexer.01tools.com", "/assets/img/jackett.png"),
-	new service("PiHole", "https://dns.01tools.com/admin", "/assets/img/pihole.png"),
-	new service("Traefik", "https://proxy.01tools.com/", "/assets/img/traefik.png")
+	new service("Plex ", "https://videos.01tools.com/web/index.html#!/dashboard", "/assets/img/plex.ico" ),
+	new service("Sonarr ","https://tv.01tools.com", "/assets/img/sonarr.ico"),
+	new service("Radarr ", "https://movies.01tools.com", "/assets/img/radarr.png"),
+	new service("Deluge ", "https://azeric.lw815.usbx.me/deluge", "/assets/img/deluge.png"),
+	new service("Home Assistant ", "https://iot.01tools.com", "/assets/img/homeassistant.png"),
+	new service("Motioneye ", "https://camera.01tools.com", "/assets/img/motioneye.png"),
+	new service("Node Red ", "https://automate.01tools.com", "/assets/img/node-red.png"),
+	new service("NextCloud ", "https://cloud.01tools.com", "/assets/img/nextcloud.png"),	
+	new service("Bitwarden ", "https://pass.01tools.com/", "/assets/img/bitwarden.png"),
+	new service("Authelia ", "https://login.01tools.com/", "/assets/img/authelia.png"),
+	new service("Jackett ", "https://indexer.01tools.com", "/assets/img/jackett.png"),
+	new service("PiHole ", "https://dns.01tools.com/admin", "/assets/img/pihole.png"),
+	new service("Traefik ", "https://proxy.01tools.com/", "/assets/img/traefik.png")
 );
 ?>
 <table class ="center">
