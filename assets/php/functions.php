2cc7afdeeb3def184b6432a0a1f7d042862aa652
Eric Charlson <Eric.Charlson@confirmit.com>
2020-10-07 12:53:15 -0400

    a

--- assets/php/functions.php
+++ assets/php/functions.php
@@ -378,7 +378,7 @@ function plexMovieStats()
 	// curl -H "Content-Length: 0" -H "X-Plex-Client-Identifier: my-app" -u "erc_c@hotmail.com"":""password" -X POST https://my.plexapp.com/users/sign_in.xml
 
 	$myPlex = shell_exec('curl -H "Content-Length: 0" -H "X-Plex-Client-Identifier: my-app" -u "'.$plex_username.'"":""'.$plex_password.'" -X POST https://my.plexapp.com/users/sign_in.xml 2> /dev/null');
-    $myPlexArray = explode("" ", $myPlex);
+    $myPlexArray = explode(" ", $myPlex);
     $token = $myPlex_xml['authentication-token'];
 	
 
