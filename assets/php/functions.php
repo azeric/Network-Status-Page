fa4470d64c4d7baea2bee586348f8cf47a6dacf6
Eric Charlson <Eric.Charlson@confirmit.com>
2020-10-07 16:56:14 -0400

    a

--- assets/php/functions.php
+++ assets/php/functions.php
@@ -376,19 +376,13 @@ function plexMovieStats()
 	global $plex_password;
 	//To test manually:
 	// curl -H "Content-Length: 0" -H "X-Plex-Client-Identifier: my-app" -u "erc_c@hotmail.com"":""password" -X POST https://my.plexapp.com/users/sign_in.xml
-		
-	$myPlex = shell_exec('curl -H "Content-Length: 0" -H "X-Plex-Client-Identifier: dashboard1" -H "X-Plex-Product: My Cool Plex App" -u "'.$plex_username.'"":""'.$plex_password.'" -X POST https://my.plexapp.com/users/sign_in.xml 2> /dev/null');
-	$myPlex_xml = simplexml_load_string($myPlex);
-	$token = $myPlex_xml['authentication-token'];
-	
 	$plexNewMoviesXML = simplexml_load_file($plex_server_ip.'/library/sections/1/all?X-Plex-Token='.$plexToken);
 	$total_movies = count($plexNewMoviesXML -> Video);
 	$plexNewTVXML = simplexml_load_file($plex_server_ip.'/library/sections/2/all?X-Plex-Token='.$plexToken);
 	$total_tv = count($plexNewTVXML -> Directory);
 	
 	echo '<div class="exolight">';
-	echo '<h4 class="exoextralight">Token: '.$myPlex_xml['authenticationToken'].'</h4>';
-	echo '<h4 class="exoextralight">Token: '.getPlexToken().'</h4>';
+	//echo '<h4 class="exoextralight">Token: '.getPlexToken().'</h4>';
 	echo '<h4 class="exoextralight">New Movies: '.$total_movies.'</h4>';
 	echo '<h4 class="exoextralight">New TV Shows: '.$total_tv.'</h4>';
 	echo '</div>';
