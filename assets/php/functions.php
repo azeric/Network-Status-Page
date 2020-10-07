22205f905b733b8946d0d1d2f6d8d0bba16e5536
Eric Charlson <Eric.Charlson@confirmit.com>
2020-10-07 16:00:14 -0400

    a

--- assets/php/functions.php
+++ assets/php/functions.php
@@ -393,7 +393,7 @@ function plexMovieStats()
 	echo '<h4 class="exoextralight">XML Token: '.$myPlex.'</h4>';
 	echo '<h4 class="exoextralight">XML Token: '.$myPlexArray[1].'</h4>';
 	echo '<h4 class="exoextralight">Plex Token: '.$chunks.'</h4>';
-	echo '<h4 class="exoextralight">Plex Token: '.$myPlex_xml[user].'</h4>';
+	echo '<h4 class="exoextralight">Plex Token: '.$myPlex_xml['user'].'</h4>';
 	echo '<h4 class="exoextralight">New Movies: '.$total_movies.'</h4>';
 	echo '<h4 class="exoextralight">New TV Shows: '.$total_tv.'</h4>';
 	echo '</div>';
