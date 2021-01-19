0e7de1e33658f54076a2406ba01f7eb3ff0097d0
Eric Charlson <Eric.Charlson@confirmit.com>
2021-01-19 13:38:49 -0500

    update widget

--- index.php
+++ index.php
@@ -295,8 +295,7 @@
 								</div>	
 								<!--*************Goodreads***************-->
 								
-<div id="gr_challenge_11650" style="border: 2px solid #EBE8D5; border-radius:10px; padding: 0px 7px 0px 7px; max-width:230px; min-height: 100px">
-  <div id="gr_challenge_progress_body_11650" style="font-size: 12px; font-family: georgia,serif;line-height: 18px">
+<div>
     <h3 style="margin: 4px 0 10px; font-weight: normal; text-align: center">
       <a style="text-decoration: none; font-family:georgia,serif;font-style:italic; font-size: 1.1em" rel="nofollow" href="https://www.goodreads.com/challenges/11650-2021-reading-challenge">2021 Reading Challenge</a>
     </h3>
@@ -320,7 +319,7 @@
         </div>
   </div>
 	<script src="https://www.goodreads.com/user_challenges/widget/11098370-eric?challenge_id=11650&v=2"></script>
-</div>
+
 
 								<div>
 								
