9f69f550fa98d774e1c16fba8a5c266e89ec804e
Eric Charlson <Eric.Charlson@confirmit.com>
2021-01-19 13:31:31 -0500

    add goodreads

--- index.php
+++ index.php
@@ -293,7 +293,145 @@
 								<div class="panel-heading">
 									<h4 class="panel-title exoextralight">Goodreads</h4>
 								</div>	
-								<div id="plex_movie_stats"></div>
+								<div>      <!-- Show static HTML/CSS as a placeholder in case js is not enabled - javascript include will override this if things work -->
+      <style type="text/css" media="screen">
+  .gr_custom_container_1611070899 {
+    /* customize your Goodreads widget container here*/
+    border: 1px solid gray;
+    border-radius:10px;
+    padding: 10px 5px 10px 5px;
+    background-color: #FFFFFF;
+    color: #000000;
+    width: 300px
+  }
+  .gr_custom_header_1611070899 {
+    /* customize your Goodreads header here*/
+    border-bottom: 1px solid gray;
+    width: 100%;
+    margin-bottom: 5px;
+    text-align: center;
+    font-size: 120%
+  }
+  .gr_custom_each_container_1611070899 {
+    /* customize each individual book container here */
+    width: 100%;
+    clear: both;
+    margin-bottom: 10px;
+    overflow: auto;
+    padding-bottom: 4px;
+    border-bottom: 1px solid #aaa;
+  }
+  .gr_custom_book_container_1611070899 {
+    /* customize your book covers here */
+    overflow: hidden;
+    height: 60px;
+      float: left;
+      margin-right: 4px;
+      width: 39px;
+  }
+  .gr_custom_author_1611070899 {
+    /* customize your author names here */
+    font-size: 10px;
+  }
+  .gr_custom_tags_1611070899 {
+    /* customize your tags here */
+    font-size: 10px;
+    color: gray;
+  }
+  .gr_custom_rating_1611070899 {
+    /* customize your rating stars here */
+    float: right;
+  }
+</style>
+
+      <div id="gr_custom_widget_1611070899">
+          <div class="gr_custom_container_1611070899">
+    <h2 class="gr_custom_header_1611070899">
+    <a style="text-decoration: none;" rel="nofollow" href="https://www.goodreads.com/review/list/11098370-eric?shelf=currently-reading&amp;utm_medium=api&amp;utm_source=custom_widget">Eric&#39;s bookshelf: currently-reading</a>
+    </h2>
+      <div class="gr_custom_each_container_1611070899">
+          <div class="gr_custom_book_container_1611070899">
+            <a title="Central Station" rel="nofollow" href="https://www.goodreads.com/review/show/1626833162?utm_medium=api&amp;utm_source=custom_widget"><img alt="Central Station" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1460506349l/25986774._SY75_.jpg" /></a>
+          </div>
+          <div class="gr_custom_rating_1611070899">
+            <span class=" staticStars notranslate"><img src="https://www.goodreads.com/images/layout/gr_red_star_inactive.png" /><img alt="" src="https://www.goodreads.com/images/layout/gr_red_star_inactive.png" /><img alt="" src="https://www.goodreads.com/images/layout/gr_red_star_inactive.png" /><img alt="" src="https://www.goodreads.com/images/layout/gr_red_star_inactive.png" /><img alt="" src="https://www.goodreads.com/images/layout/gr_red_star_inactive.png" /></span>
+          </div>
+          <div class="gr_custom_title_1611070899">
+            <a rel="nofollow" href="https://www.goodreads.com/review/show/1626833162?utm_medium=api&amp;utm_source=custom_widget">Central Station</a>
+          </div>
+          <div class="gr_custom_author_1611070899">
+            by <a rel="nofollow" href="https://www.goodreads.com/author/show/572738.Lavie_Tidhar">Lavie Tidhar</a>
+          </div>
+          <div class="gr_custom_tags_1611070899">
+            tagged:
+            currently-reading
+          </div>
+      </div>
+      <div class="gr_custom_each_container_1611070899">
+          <div class="gr_custom_book_container_1611070899">
+            <a title="Helping Your Child with Extreme Picky Eating: A Step-by-Step Guide for Overcoming Selective Eating, Food Aversion, and Feeding Disorders" rel="nofollow" href="https://www.goodreads.com/review/show/3394970286?utm_medium=api&amp;utm_source=custom_widget"><img alt="Helping Your Child with Extreme Picky Eating: A Step-by-Step Guide for Overcoming Selective Eating, Food Aversion, and Feeding Disorders" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1413517246l/23129657._SX50_.jpg" /></a>
+          </div>
+          <div class="gr_custom_rating_1611070899">
+            <span class=" staticStars notranslate"><img src="https://www.goodreads.com/images/layout/gr_red_star_inactive.png" /><img alt="" src="https://www.goodreads.com/images/layout/gr_red_star_inactive.png" /><img alt="" src="https://www.goodreads.com/images/layout/gr_red_star_inactive.png" /><img alt="" src="https://www.goodreads.com/images/layout/gr_red_star_inactive.png" /><img alt="" src="https://www.goodreads.com/images/layout/gr_red_star_inactive.png" /></span>
+          </div>
+          <div class="gr_custom_title_1611070899">
+            <a rel="nofollow" href="https://www.goodreads.com/review/show/3394970286?utm_medium=api&amp;utm_source=custom_widget">Helping Your Child with Extreme Picky Eating: A Step-by-Step Guide for Overcoming Selective Eating, Food Aversion, and Feeding Disorders</a>
+          </div>
+          <div class="gr_custom_author_1611070899">
+            by <a rel="nofollow" href="https://www.goodreads.com/author/show/6535908.Katja_Rowell">Katja Rowell</a>
+          </div>
+          <div class="gr_custom_tags_1611070899">
+            tagged:
+            currently-reading
+          </div>
+      </div>
+      <div class="gr_custom_each_container_1611070899">
+          <div class="gr_custom_book_container_1611070899">
+            <a title="Sensational Kids: Hope and Help for Children with Sensory Processing Disorder" rel="nofollow" href="https://www.goodreads.com/review/show/3626156393?utm_medium=api&amp;utm_source=custom_widget"><img alt="Sensational Kids: Hope and Help for Children with Sensory Processing Disorder" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1309281980l/880956._SY75_.jpg" /></a>
+          </div>
+          <div class="gr_custom_rating_1611070899">
+            <span class=" staticStars notranslate"><img src="https://www.goodreads.com/images/layout/gr_red_star_inactive.png" /><img alt="" src="https://www.goodreads.com/images/layout/gr_red_star_inactive.png" /><img alt="" src="https://www.goodreads.com/images/layout/gr_red_star_inactive.png" /><img alt="" src="https://www.goodreads.com/images/layout/gr_red_star_inactive.png" /><img alt="" src="https://www.goodreads.com/images/layout/gr_red_star_inactive.png" /></span>
+          </div>
+          <div class="gr_custom_title_1611070899">
+            <a rel="nofollow" href="https://www.goodreads.com/review/show/3626156393?utm_medium=api&amp;utm_source=custom_widget">Sensational Kids: Hope and Help for Children with Sensory Processing Disorder</a>
+          </div>
+          <div class="gr_custom_author_1611070899">
+            by <a rel="nofollow" href="https://www.goodreads.com/author/show/453532.Lucy_Jane_Miller">Lucy Jane Miller</a>
+          </div>
+          <div class="gr_custom_tags_1611070899">
+            tagged:
+            currently-reading
+          </div>
+      </div>
+      <div class="gr_custom_each_container_1611070899">
+          <div class="gr_custom_book_container_1611070899">
+            <a title="Hench" rel="nofollow" href="https://www.goodreads.com/review/show/3639374642?utm_medium=api&amp;utm_source=custom_widget"><img alt="Hench" border="0" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1594616305l/49867430._SY75_.jpg" /></a>
+          </div>
+          <div class="gr_custom_rating_1611070899">
+            <span class=" staticStars notranslate"><img src="https://www.goodreads.com/images/layout/gr_red_star_inactive.png" /><img alt="" src="https://www.goodreads.com/images/layout/gr_red_star_inactive.png" /><img alt="" src="https://www.goodreads.com/images/layout/gr_red_star_inactive.png" /><img alt="" src="https://www.goodreads.com/images/layout/gr_red_star_inactive.png" /><img alt="" src="https://www.goodreads.com/images/layout/gr_red_star_inactive.png" /></span>
+          </div>
+          <div class="gr_custom_title_1611070899">
+            <a rel="nofollow" href="https://www.goodreads.com/review/show/3639374642?utm_medium=api&amp;utm_source=custom_widget">Hench</a>
+          </div>
+          <div class="gr_custom_author_1611070899">
+            by <a rel="nofollow" href="https://www.goodreads.com/author/show/20563409.Natalie_Zina_Walschots">Natalie Zina Walschots</a>
+          </div>
+          <div class="gr_custom_tags_1611070899">
+            tagged:
+            currently-reading
+          </div>
+      </div>
+  <br style="clear: both"/>
+  <center>
+    <a rel="nofollow" href="https://www.goodreads.com/"><img alt="goodreads.com" style="border:0" src="https://www.goodreads.com/images/widget/widget_logo.gif" /></a>
+  </center>
+  <noscript>
+    Share <a rel="nofollow" href="https://www.goodreads.com/">book reviews</a> and ratings with Eric, and even join a <a rel="nofollow" href="https://www.goodreads.com/group">book club</a> on Goodreads.
+  </noscript>
+  </div>
+
+      </div>
+      <script src="https://www.goodreads.com/review/custom_widget/11098370.Eric's%20bookshelf:%20currently-reading?cover_position=left&cover_size=small&num_books=5&order=a&shelf=currently-reading&show_author=1&show_cover=1&show_rating=1&show_review=1&show_tags=1&show_title=1&sort=date_added&widget_bg_color=FFFFFF&widget_bg_transparent=&widget_border_width=1&widget_id=1611070899&widget_text_color=000000&widget_title_size=medium&widget_width=medium" type="text/javascript" charset="utf-8"></script></div>
 							</div>
 					</div>
 				</div>
