6b61ef3e3860079a39f1ac9747f51e291eb730f0
Eric Charlson <Eric.Charlson@confirmit.com>
2021-02-09 15:18:17 -0500

    latest bootstrap

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 		<!-- Le styles -->
 		<link href="assets/fonts/stylesheet.css" rel="stylesheet">
 		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0-wip/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 		<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.0/css/font-awesome.css" rel="stylesheet">
 		<style type="text/css">
 			body {
