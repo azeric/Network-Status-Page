c9ce0a7c1b8daa2b54abc32d81f82727f18dd752
Eric Charlson <Eric.Charlson@confirmit.com>
2021-02-11 16:49:43 -0500

    test

--- index.php
+++ index.php
@@ -224,7 +224,7 @@
 							<div class="panel panel-default">
 							<div class="panel-heading">
 									<h4 class="panel-title exoextralight">
-										Weather TEST sync
+										Weather test
 									</h4>
 							</div>		
 								<div class="panel-body">	
